a4e2c75a9c0986846d7c7237b55ad942e3608ced
Timothy Sit <timothysit@Timothys-MacBook-Pro.local>
2022-01-07 21:59:08 +0000

    replaced 25000 with Params.fs

--- Functions/electrodeHeatMaps.m
+++ Functions/electrodeHeatMaps.m
@@ -1,8 +1,29 @@
 function electrodeHeatMaps(FN,spikeMatrix,channels,spikeFreqMax,Params)
+%{
+
+INPUT
+-----------
+
+FN : ? 
+spikeMatrix : ? 
+channels : ? 
+spikeFreqMax : ? 
+Params : (struct)
+
+
+OUTPUT 
+-----------
+
+
+
+%}
 
 %% create channels variable if it doesn't exist
 if ~exist('channels')
-       channels = [47,48,46,45,38,37,28,36,27,17,26,16,35,25,15,14,24,34,13,23,12,22,33,21,32,31,44,43,41,42,52,51,53,54,61,62,71,63,72,82,73,83,64,74,84,85,75,65,86,76,87,77,66,78,67,68,55,56,58,57];
+       channels = [47,48,46,45,38,37,28,36,27,17,26,16, ...
+           35,25,15,14,24,34,13,23,12,22,33,21,32,31,44, ... 
+           43,41,42,52,51,53,54,61,62,71,63,72,82,73,83,64, ...
+           74,84,85,75,65,86,76,87,77,66,78,67,68,55,56,58,57];
 end
 
 %% plot
@@ -22,7 +43,7 @@ yc = coords(:,2);
 %% calculate spiking frequency
 
 spikeCount = sum(spikeMatrix); 
-spikeCount = full(spikeCount / (size(spikeMatrix, 1) / 25000));
+spikeCount = full(spikeCount / (size(spikeMatrix, 1) / Params.fs));
 
 %% plot electrodes
 
