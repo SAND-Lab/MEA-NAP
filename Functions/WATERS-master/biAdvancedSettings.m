6ea2baad4e59bfb89cdada53098beef4baba2a06
Timothy Sit <timothysit@Timothys-MacBook-Pro.local>
2022-08-10 21:48:45 +0100

    removed loading of default parameter file

--- Functions/WATERS-master/biAdvancedSettings.m
+++ Functions/WATERS-master/biAdvancedSettings.m
@@ -1,11 +1,4 @@
 %% Advanced settings for batchInterface (only modify if you know what you are doing)
-load params
-FNames = fieldnames(params);
-for i = 1:length(FNames)
-    VN = char(FNames{i});
-    eval(['Params.' VN '= params.' VN ';']);
-end
-clear params
 
 %% Spike detection settings
 Params.plotDetectionResults = 0;
@@ -64,8 +57,6 @@ option = 'list';
 % 99 : prevent outliers dominating the plot, 100 : scale to the max (for
 % low firing rate)
 Params.rasterPlotUpperPercentile = 99;  
-
-
 
 %% Network analysis settings 
 Params.netMetToCal = {'ND', 'EW', 'NS', 'aN', 'Dens', 'Ci', 'Q', 'nMod', 'Eglob', ...,
@@ -203,7 +194,22 @@ end
 
 Params.coords  = Params.coords * 8;  % Do not remove this line after specifying coordinate positions in (0 - 1 format)
 num_channels = size(Params.coords, 1);
-figure;
+
+%% Plot electrode layout
+p = [100 100 1400 550];
+if Params.showOneFig 
+    if ~isfield(Params, 'oneFigure')
+        Params.oneFigure = figure;
+    end 
+    set(0, 'DefaultFigurePosition', p)
+    set(Params.oneFigure, 'Position', p);
+else
+    F1 = figure;
+    set(0, 'DefaultFigurePosition', p)
+    set(F1, 'Position', p);
+end 
+
+
 subplot(1, 2, 1)
 title('Provided electrode layout (channel index)')
 for n_channel = 1:num_channels
@@ -227,4 +233,25 @@ xlim([min(Params.coords(:, 1)) - 1, max(Params.coords(:, 1) + 1)])
 ylim([min(Params.coords(:, 2)) - 1, max(Params.coords(:, 2) + 1)])
 
 set(gcf, 'color', 'w')
+
+% save figure
+figFolder = fullfile(HomeDir, strcat(['OutputData', Params.Date]));
+if ~isfolder(figFolder)
+    mkdir(figFolder)
+end 
+figName = 'channel_layout';
+figSavePath = fullfile(figFolder, figName);
+
+if ~isfield(Params, 'oneFigure')
+    pipelineSaveFig(figSavePath, Params.figExt, Params.fullSVG, F1);
+else 
+    pipelineSaveFig(figSavePath, Params.figExt, Params.fullSVG, Params.oneFigure);
+end 
+
+if ~isfield(Params, 'oneFigure')
+    close all
+else 
+    set(0, 'CurrentFigure', Params.oneFigure);
+    clf reset
+end 
 
