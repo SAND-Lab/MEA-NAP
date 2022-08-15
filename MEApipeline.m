51773af7a8680b0736090686815dcff0087d2479
Timothy Sit <timothysit@eduroam-int-dhcp-97-218-80.ucl.ac.uk>
2022-08-15 21:40:31 +0100

    added intepreting 4th column as char in spreadsheet

--- MEApipeline.m
+++ MEApipeline.m
@@ -135,7 +135,8 @@ elseif strcmp(spreadsheet_file_type, 'csv')
     opts.VariableNamesLine = 1;
     opts.VariableTypes{1} = 'char';  % this should be the recoding file name
     opts.VariableTypes{2} = 'double';  % this should be the DIV
-    opts.VariableTypes{3} = 'char';
+    opts.VariableTypes{3} = 'char'; % this should be Group 
+    opts.VariableTypes{4} = 'char'; % this should be Ground
     opts.DataLines = [2 Inf];  % start reading data from row 2
     % csv_data = readtable(spreadsheet_filename, 'Delimiter','comma');
     csv_data = readtable(spreadsheet_filename, opts);
