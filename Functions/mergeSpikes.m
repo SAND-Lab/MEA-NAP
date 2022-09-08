7417c0dc34c18d1b85860803f54943300e4cc480
Timothy Sit <timothysit@Timothys-MacBook-Pro.local>
2022-09-08 13:23:36 +0100

    wavelet merge now search through available wavelets first

--- Functions/mergeSpikes.m
+++ Functions/mergeSpikes.m
@@ -1,40 +1,45 @@
 function [all_spikes, unique_idx, intersect_matrix] = mergeSpikes(spike_times, option)
-% Description: Merges spike times across detection methods.
-%----------
-% INPUT
-% spike_times - [n x 1 cell] containing for each electrode a structure
+% Merges spike times across detection methods.
+% TIP: to get spike waveforms corresponding to the merged spike times call e.g.:
+%      [~, spikes] = alignPeaks(spike_times.all, filtered_trace, 25, 0, 1, 10, 10);
+% Parameters
+% ----------
+% spike_times - [n x 1 cell] containing for each electrode a structure : TODO : I
+% think this is a structure (???)
 % option      - [string] specifies merging option: 'all' or 'wavelets'; if
 %                        not specified, defaults to 'all'
-%----------
-% OUTPUT
+% Returns 
+% -------
 % all_spikes  - [struct] with merged spike times appended to spike_times{channel}.('all')
 % unique_idx - [vector] containing the indexes of spikes detected uniquely by a given method
+% 
 % NOTE: the number in unique_idx corresponds to the position in fieldnames
 %       of spike_times, e.g. if field no. 1 is 'mea', then 1 in unique_idx
 %       will represent a spike detected uniquely by 'mea' method
 %       Zero represents spikes picked up by at least 2 methods
-
-% TIP: to get spike waveforms corresponding to the merged spike times call e.g.:
-%      [~, spikes] = alignPeaks(spike_times.all, filtered_trace, 25, 0, 1, 10, 10);
-%----------
+% TODO: I think this merging method does not currently merge within a
+% certain time bin (eg. combine spikes if they are in the same 5 ms time
+% bin?)
 % @author JJChabros (jjc80@cam.ac.uk), January 2021
+% Updates by Tim Sit
+% 2022-09 : 'wavelets' merge takes in availble wavelets rather than having
+% the user to use all the wavelets
 
 switch option
     case 'all'
         methods = fieldnames(spike_times);
     case 'wavelets'
-        methods = {'mea','bior1p5','bior1p3','db2'};
-end
-
-if ~exist('option', 'var')
-    option = 'all';
+        methods_to_search = {'mea','bior1p5','bior1p3','db2'};
+        methods = intersect(methods_to_search, fieldnames(spike_times));
 end
 
 all_spikes = [];
+
 for method = 1:numel(methods)
 %     all_spikes = union(all_spikes, round(spike_times.(methods{method})*25000));
-all_spikes = union(all_spikes, spike_times.(methods{method}));
+    all_spikes = union(all_spikes, spike_times.(methods{method}));
 end
+
 all_spikes = all_spikes';
 % Vectorized operations + Logical indexing = Fast but hard to follow
 
