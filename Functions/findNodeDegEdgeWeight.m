2d4b52ae7cd95478c7a1d3942161f9dfe336ab19
Timothy Sit <timothysit@Timothys-MacBook-Pro.local>
2022-10-07 13:55:46 +0100

    by default now excludes links with weight 0 when calculating mean edge weight

--- Functions/findNodeDegEdgeWeight.m
+++ Functions/findNodeDegEdgeWeight.m
@@ -1,26 +1,28 @@
-function [z,z2] = findNodeDegEdgeWeight(adjM, edge_thresh)
-%{ 
-find node degree and edge weight from adjacency matrix
-INPUT 
-------------------
-adjM : (matrix)
-    adjacency matrix of the network 
-edge_thresh : (float or vector)
-    this is a vector (list) of thresholds (between 0 and 1) to be
-    used for thresholding the edges of the network.
-    
+function [z,z2] = findNodeDegEdgeWeight(adjM, edge_thresh, exclude_zeros)
+% Calculates mean node degree and mean edge weight from adjacency matrix
+% Parameters 
+% ----------
+% adjM : (matrix)
+%     adjacency matrix of the network 
+% edge_thresh : (float or vector)
+%     this is a vector (list) of thresholds (between 0 and 1) to be
+%     used for thresholding the edges of the network. Values below (including
+%     negative values) this threshold will be set to zero
+% exclude_zeros : bool 
+%     whether to exclude zeros when calculating the mean edge weight 
+%     defaults to True
+% Returns 
+% -------
+% z : mean node degree (often referred to as ND), this is the degree of each node 
+%     this is the mean degree of each node, rounded to the nearest integer.
+% z2 : mean edge weight (often referred to as EW), this is the edge weight of each
+%     this is not rounded, note that negative weights are thresholded to zero
+%   
 
-OUTPUT 
-------------------
-z : mean node degree (often referred to as ND), this is the degree of each node 
-    this is the mean degree of each node, rounded to the nearest integer.
-z2 : mean edge weight (often referred to as EW), this is the edge weight of each
-    this is not rounded, note that negative weights are thresholded to zero
-   
-node
+if nargin == 2
+    exclude_zeros = 1;
+end 
 
-
-%}
 %% get node degree for each channel
 
 count = 1; %to track threshold iterations
@@ -46,6 +48,11 @@ weights = adjM;
 weights = weights - eye(size(weights));
 weights(find(isnan(weights))) = 0;
 weights(find(weights < 0)) = 0;
-z2 = mean(weights)';
+
+if exclude_zeros
+    weights(weights == 0) = nan;
+end 
+
+z2 = nanmean(weights)';
 
 end
