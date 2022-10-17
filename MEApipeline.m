b1d6ab712d094dfba706b3659211e13426991925
Synapse and Network Development Group <86618605+SAND-Group@users.noreply.github.com>
2022-10-17 17:20:22 -0400

    Copy edit text for GUI

--- MEApipeline.m
+++ MEApipeline.m
@@ -74,19 +74,19 @@ if Params.guiMode
     opts = struct(); 
     opts.Default = 'Okay';
     opts.Interpreter = 'tex';
-    helloBox = questdlg("\fontsize{20} Hello! Welcome to the MEA network analysis pipeline toolbox!", ...
+    helloBox = questdlg("\fontsize{20} Hello! Welcome to the MEA network analysis pipeline (MEA-NAP)!", ...
      'Welcome!', 'Okay', opts);
     clear helloBox
     
     opts = struct(); 
     opts.Default = 'Okay';
     opts.Interpreter = 'tex';
-    selectHomeDir = questdlg("\fontsize{20} First, please select the folder where your MEApieline.m script is located", ...
+    selectHomeDir = questdlg("\fontsize{20} First, please select the folder where your MEApipeline.m script is located", ...
         'Home directory selection', 'Okay', opts);
     % uiwait(selectHomeDir);
     clear selectHomeDir
 
-    homeDirUiGet = uigetdir(pwd, 'Please select the folder where the MEApipeline.m script is in');
+    homeDirUiGet = uigetdir(pwd, 'Please select the folder where the MEApipeline.m script is located');
     % uiwait(homeDirUiGet)
     Params.HomeDir = homeDirUiGet;
 
@@ -96,19 +96,19 @@ if Params.guiMode
         'Raw data folder', 'Okay', opts);
     clear rawDataAnswer
     
-    rawDataUIGet = uigetdir(pwd, 'Please select the folder where the raw data is in');
+    rawDataUIGet = uigetdir(pwd, 'Please select the folder containing your raw data');
     rawData = rawDataUIGet;
     clear rawDataUIGet
 
     % Please select the spreadsheet containing the set of recordings 
-    spreadSheetAnswer = questdlg("\fontsize{20} Please select the spreadsheet containing a list of files you want to run the pipeline", ...
+    spreadSheetAnswer = questdlg("\fontsize{20} Please select the spreadsheet containing the list of files you want to run the pipeline", ...
         'Spreadsheet file', 'Okay', opts);
     spreadsheet_filename = uigetfile(pwd, 'Please select the spreadsheet');
 
     % Asking user if they are running pipeline the first time on raw data
     firstTimeOrNo = {'Yes', 'No'};
     [indx,tf] = listdlg('PromptString',{'Are you running this pipeline for' ...
-        'the first time on raw data?'}, ...
+        'the first time on this raw data?'}, ...
              'ListString',firstTimeOrNo);
 
     runningPipelineFirstTime = firstTimeOrNo{indx};
@@ -121,8 +121,8 @@ if Params.guiMode
     elseif strcmp(runningPipelineFirstTime, 'No')
          Params.priorAnalysis = 1;
          % ask user which step they want to start the pipeline on 
-         availStartAnalysisSteps = {'1 : Spike detection', '2 : neuronal activity', ...
-             '3 : functional connectivity', '4 : network activity', '5 : stats and classification'};
+         availStartAnalysisSteps = {'1 : Spike detection', '2 : Neuronal activity', ...
+             '3 : Functional connectivity', '4 : Network activity', '5 : Stats and classification'};
          [indx, tf] = listdlg('PromptString',{'Please select which step you want' ...
              'to start the pipeline'}, ...
              'ListString',availStartAnalysisSteps);
@@ -148,11 +148,11 @@ if Params.guiMode
     
     % Ready to start pipeline 
     opts = struct();
-    opts.Default = 'Run pipeline!';
+    opts.Default = 'Run MEA-NAP!';
     opts.Interpreter = 'tex';
     pause(0.1);
-    readyBox = questdlg("\fontsize{20} We are all set!", ...
-     'Ready!', 'Run pipeline!', opts);
+    readyBox = questdlg("\fontsize{20} You are all set!", ...
+     'Ready!', 'Run MEA-NAP!', opts);
     drawnow; pause(0.1);
 
     clear readyBox
