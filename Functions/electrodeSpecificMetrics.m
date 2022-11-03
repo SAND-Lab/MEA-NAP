90f745a3e8843ddf84d4352fa3a54e4912080a34
Timothy Sit <timothysit@eduroam-int-dhcp-97-250-141.ucl.ac.uk>
2022-11-03 15:21:44 +0000

    dealt with cases where network metrics is single element nan

--- Functions/electrodeSpecificMetrics.m
+++ Functions/electrodeSpecificMetrics.m
@@ -73,53 +73,54 @@ set(gca,'xtick',[])
 ylabel('node strength')
 ylim([0 max(NS)+0.2*max(NS)])
 
+% Plot within-module degree z-score
 nexttile(11,[3,1])
-HalfViolinPlot(Z,1,[0.3 0.3 0.3],0.3)
-aesthetics
-set(gca,'TickDir','out');
-set(gca,'xtick',[])
-ylabel('within-module degree z-score')
-try
-    ylim([min(Z)-abs(min(Z))*0.2 max(Z)+0.2*max(Z)])
-catch
-    ylim([0 1])
-end
 
+skipPlot = (numel(Z) == 1) && isnan(Z);
+if ~skipPlot
+    HalfViolinPlot(Z,1,[0.3 0.3 0.3],0.3)
+    aesthetics
+    set(gca,'TickDir','out');
+    set(gca,'xtick',[])
+    ylabel('within-module degree z-score')
+    ylim([nanmin(Z)-abs(nanmin(Z))*0.2 nanmax(Z)+0.2*nanmax(Z)])
+end 
+
+% Plot local efficiency
 nexttile(12,[3,1])
-HalfViolinPlot(Eloc,1,[0.3 0.3 0.3],0.3)
-aesthetics
-set(gca,'TickDir','out');
-set(gca,'xtick',[])
-ylabel('local connectivity')
-try
-    ylim([0 max(Eloc)+0.2*max(Eloc)])
-catch
-    ylim([0 1])
-end
+skipPlot = (numel(Eloc) == 1) && isnan(Eloc);
+if ~skipPlot
+    HalfViolinPlot(Eloc,1,[0.3 0.3 0.3],0.3)
+    aesthetics
+    set(gca,'TickDir','out');
+    set(gca,'xtick',[])
+    ylabel('local connectivity')
+    ylim([0 nanmax(Eloc)+0.2*nanmax(Eloc)])
+end 
 
+% Plot participation coefficient 
 nexttile(13,[3,1])
-HalfViolinPlot(PC,1,[0.3 0.3 0.3],0.3)
-aesthetics
-set(gca,'TickDir','out');
-set(gca,'xtick',[])
-ylabel('participation coefficient')
-try
-    ylim([0 max(PC)+0.2*max(PC)])
-catch
-    ylim([0 1])
-end
+skipPlot = (numel(PC) == 1) && isnan(PC);
+if ~skipPlot
+    HalfViolinPlot(PC,1,[0.3 0.3 0.3],0.3)
+    aesthetics
+    set(gca,'TickDir','out');
+    set(gca,'xtick',[])
+    ylabel('participation coefficient')
+    ylim([0 nanmax(PC)+0.2*nanmax(PC)])
+end 
 
+% Plot betweeness centrality 
+skipPlot = (numel(BC) == 1) && isnan(BC);
 nexttile(14,[3,1])
-HalfViolinPlot(BC,1,[0.3 0.3 0.3],0.3)
-aesthetics
-set(gca,'TickDir','out');
-set(gca,'xtick',[])
-ylabel('betweeness centrality')
-try
-    ylim([0 max(BC)+0.2*max(BC)])
-catch
-    ylim([0 0.1])
-end
+if ~skipPlot
+    HalfViolinPlot(BC,1,[0.3 0.3 0.3],0.3)
+    aesthetics
+    set(gca,'TickDir','out');
+    set(gca,'xtick',[])
+    ylabel('betweeness centrality')
+    ylim([0 nanmax(BC)+0.2*nanmax(BC)])
+end 
 
 
 %% save figure
