faeb741e832e55a8b92954eb496020291ac9d9f8
timothysit <sitpakhang@gmail.com>
2023-03-03 19:02:43 +0000

    returns figure handle for making combined plots

--- Functions/StandardisedNetworkPlotNodeColourMap.m
+++ Functions/StandardisedNetworkPlotNodeColourMap.m
@@ -1,4 +1,4 @@
-function [] = StandardisedNetworkPlotNodeColourMap(adjM, coords, edge_thresh, z, zname, z2, z2name, plotType, ...
+function [figureHandle, cb] = StandardisedNetworkPlotNodeColourMap(adjM, coords, edge_thresh, z, zname, z2, z2name, plotType, ...
                                                    FN, pNum, Params, lagval, e, figFolder)
 % Plots graph network with node size proportional to some node-level variable of
 % choice and color-mapped based on some other node-level variable of choice
@@ -488,6 +488,15 @@ if ~isfield(Params, 'oneFigure')
     pipelineSaveFig(figPath, Params.figExt, Params.fullSVG, F1);
 else 
     pipelineSaveFig(figPath, Params.figExt, Params.fullSVG, Params.oneFigure);
+end 
+
+
+
+%  output figure handle 
+if ~isfield(Params, 'oneFigure')
+    figureHandle = F1;
+else 
+    figureHandle = Params.oneFigure;
 end 
 
 
