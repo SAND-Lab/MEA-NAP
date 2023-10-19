2d67f0c1a5b0aa824c54695a146e0f0f492ef5eb
timothysit <sitpakhang@gmail.com>
2023-10-19 11:49:54 +0100

    plotting residual and var explained

--- Functions/plotNMF.m
+++ Functions/plotNMF.m
@@ -1,6 +1,7 @@
 function plotNMF(experimentMatFolder, plotSaveFolder, Params)
     
-    
+    % Plot NMF components
+    nCompToPlotIfAvail = 3;
 
     fileList = dir(fullfile(experimentMatFolder, '*.mat'));
 
@@ -14,31 +15,68 @@ function plotNMF(experimentMatFolder, plotSaveFolder, Params)
         firstLagFieldData = NetMetData.(lagFields{1});
         
         nCompsAvailable = size(firstLagFieldData.nmfFactors, 2);
-        nCompToPlot = 3;
 
-        if nCompsAvailable < nCompToPlot
+        if nCompsAvailable < nCompToPlotIfAvail
             nCompToPlot = nCompsAvailable;
+        else 
+            nCompToPlot = nCompToPlotIfAvail;
         end 
-
+        
+        max_spike_count = max(max(firstLagFieldData.downSampleSpikeMatrix'));
 
         nmfFig = figure;
         
-        subplot(3, 2, 3)
-        imagesc(firstLagFieldData.downSampleSpikeMatrix');
+        subplot(3, 2, 1)
+        imagesc(firstLagFieldData.downSampleSpikeMatrix', [0, max_spike_count]);
+        colormap(flipud(gray)) 
+        title('Original activity')
+        ylabel('Channel')
+        xlabel('Time bins')
+        set(gca,'TickDir','out');
+        set(gca,'box','off')
         hold on 
         
+        subplot(3, 2, 3)
+        plot(firstLagFieldData.nnmf_var_explained, 'linewidth', 2);
+        hold on
+        numCompVarThreshold = size(firstLagFieldData.nmfWeightsVarThreshold, 1);
+        plot([numCompVarThreshold, numCompVarThreshold], [0, 1], '--', ... 
+            'linewidth', 1, 'Color',[0.5 0.5 0.5]);
+        text(numCompVarThreshold, 0.1, '95% VE', 'Color', [0.5, 0.5, 0.5])
+        xlabel('nNMF components')
+        ylabel('Variance explained')
+        
+        ylim([0, 1.1])
+        set(gca,'TickDir','out');
+        set(gca,'box','off')
+        
+        
+        
+        subplot(3, 2, 5)
+        plot(firstLagFieldData.nnmf_residuals, 'linewidth', 2);
+        xlabel('nNMF components')
+        ylabel('Mean square root residual')
+        set(gca,'TickDir','out');
+        set(gca,'box','off')
+        
+       
 
         for nComp = 1:nCompToPlot
     
             subplot(3, 2, nComp*2)
 
+            % nmfProjection = firstLagFieldData.nmfFactors(:, nComp) * ...
+            %                 firstLagFieldData.nmfWeights(nComp, :);
             nmfProjection = firstLagFieldData.nmfFactors(:, nComp) * ...
-                            firstLagFieldData.nmfWeights(nComp, :);
+                             firstLagFieldData.nmfWeights(nComp, :);
 
-            imagesc(nmfProjection')
+            imagesc(nmfProjection', [0, max_spike_count])
+            ylabel('Channel')
+            xlabel('Time bins')
+            set(gca,'TickDir','out');
+            set(gca,'box','off')
             hold on 
             
-
             title(sprintf('NMF component %.f', nComp))
 
         end 
