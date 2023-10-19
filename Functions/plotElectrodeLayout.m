797d0626e82bfbf1bc24f7f540341ff7c675f628
timothysit <sitpakhang@gmail.com>
2023-10-19 11:48:50 +0100

    now works properly with new coords and channels per recording

--- Functions/plotElectrodeLayout.m
+++ Functions/plotElectrodeLayout.m
@@ -2,7 +2,11 @@ function plotElectrodeLayout(HomeDir, Params, oneFigureHandle)
 %PLOTELECTRODELAYOUT Plots the layout of the electrodes specified in Params
 %   Detailed explanation goes here
 
-num_channels = size(Params.coords, 1);
+if iscell(Params.coords)
+    num_channels = size(Params.coords{1}, 1);
+else
+    num_channels = size(Params.coords, 1);
+end
 p = [100 100 1400 550];
 if Params.showOneFig 
     if ~isgraphics(oneFigureHandle)
