c56a39698111b8faae8ea263d62d489ce714dbda
timothysit <sitpakhang@gmail.com>
2023-11-13 14:05:58 +0000

    cleaned up code comments

--- Functions/convertRawToMat/MEA_load_bin.m
+++ Functions/convertRawToMat/MEA_load_bin.m
@@ -1,31 +1,27 @@
 function [header,m,channels] = MEA_load_bin(binfile,plt, convertOption)
-% note that I (TS) is going to modify this slightly to make it work with
-% any number of channels 
-% also changed the code so that it saves one giant variable 
-% rather than create a subfolder 
-% also makes running combineMat.m unncessary 
-
-%form: 
-%This function takes txt file output from MC_Datatool (Multichannel
-%Systems) and converts it into a matlab matrix.
-%
-
-% Instructions 
-
+% Convert bin files to mat files for MEA data
+% This function takes txt file output from MC_Datatool (Multichannel
+% Systems) and converts it into a matlab matrix.
+% Parameters 
+% -----------
+% binfile : str 
+%      path to the bin file to be converted
+% plt : bool
+%      whether to plot
+% convertOption: str
+%      'whole' - converts the bin file into one files with one matrix 
+%      'electrode' - convert the bin file into files each with one electrde
+%     
+% Notes
+% -------------
 % Do not run this directly, this is called by MEAbatchConvert.m 
 % But a lot of the key steps of the conversion processes are contained here
-% The main thing to change is 'convertOption', which you should specify in
-% MEAbatchConvert.m, the default is to do 'whole'; the entire grid will be
-% saved in one variable 
-% the other option is to do electrode by electrode conversion, in which
-% case convertOption == 'electrode'
-
-% Last update: 20180626 
-
+% 
 % Log
-% 20180626: Added option to save each electrode as an individual .mat file 
+% ---------------
+% 2018-06-26: Tim Sit - Added option to save each electrode as an individual .mat file 
 % (to avoid memory issues on systems with less than 16GB ram / low swap space)
-
+% 2023-11-13 : Tim Sit - Updated documentation
 
 %% initialize
 
@@ -49,8 +45,8 @@ fclose(fid);
 
 %% get key values from header
 
-m=m';
-f=findstr(m,'EOH');
+m = m';
+f = findstr(m,'EOH');
 newstart=f+4;
 header=m(1:f-1)
 
@@ -66,16 +62,16 @@ uV = str2num(m(uVf-7:uVf-2));
 
 %% get channels from header
 
-chf=findstr(header,'Streams');
-chs = header(chf+11:length(header))
+chf = findstr(header,'Streams');
+chs = header(chf+11:length(header));
 channels=[];
+
 while length(chs)>5
-    f=findstr(chs,'_');
-    channels=[channels; str2num(chs(f(1)+1:f(1)+2))];
+    f = findstr(chs,'_');
+    channels = [channels; str2num(chs(f(1)+1:f(1)+2))];
     chs(1:f(1)+2)=[];
-end;
-channels;
-size(channels)
+end
+
 %% get data
 
 [fid] =fopen(binfile);
@@ -88,10 +84,6 @@ fclose(fid);
 %each row is a different channel
 numChannels = length(channels); 
 m=reshape(data,numChannels,floor(length(data)/numChannels));
-% note that I (TS) chaged '60' to numChannels 
-% not sure if there will be any consequences... 
-% need to check through the logic of this code to make sure
-
 
 %% convert data to microvolts (see MC_datatool help - search 'binary')
 max(m(:));
@@ -103,21 +95,20 @@ dat = m'; % matlab reads tall matrices more efficiently
 % tall means number of rows > number of columns
 %% plot
 
-
 if plt
     ch=82;
     figure
     t=1:size(m,2);
     t=t/fs;
     size(m)
-    f=find(channels==ch)
+    f = find(channels==ch);
     plot(t,m(f(1),:),'k')
-end;
+end
 
 %% save
 sprintf('Saving data ...')
 
-% /mkdir(binfile(1:length(binfile)-4))
+% mkdir(binfile(1:length(binfile)-4))
 % the crux to not having folders is here 
 % save([binfile(1:length(binfile)-4) filesep binfile(1:length(binfile)-4) '.mat'],'dat','channels','header','uV','ADCz','fs')
 % the above line saves to specific subfolders 
