add38d879215b4796d73a1005f8e79220bd3f888
davidoluigbo25@gmail.com <doluigbo@mit.edu>
2024-02-15 09:18:23 -0500

    Updated documentation

--- Functions/McsMatlabDataTools/+McsHDF5/@McsCmosLinkedDataSource/plot.m
+++
@@ -1,8 +0,0 @@
-function plot(acq, cfg, varargin)
-% Placeholder plot function for a McsCmosLinkedDataSource object
-%
-% function plot(acq, cfg, varargin)
-%
-% This function is empty at the moment
-    
-end
