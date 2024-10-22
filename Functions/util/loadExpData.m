52e0c5f1b72b5a9c21a956f4a82d06ca916df13c
timothysit <sitpakhang@gmail.com>
2024-10-22 09:40:23 +0100

    added function to load exp data

---
+++ Functions/util/loadExpData.m
@@ -0,0 +1,16 @@
+function expData = loadExpData(ExpName, Params, usePriorNetMet)
+%LOADEXPDATA Summary of this function goes here
+%   Detailed explanation goes here
+    if Params.priorAnalysis==1 && Params.startAnalysisStep==4 && usePriorNetMet
+        experimentMatFileFolder = fullfile(Params.priorAnalysisPath, 'ExperimentMatFiles');
+        experimentMatFilePath = fullfile(experimentMatFileFolder, strcat(char(ExpName),'_',Params.priorAnalysisDate,'.mat'));
+    else
+        experimentMatFileFolder = fullfile(Params.outputDataFolder, Params.outputDataFolderName, 'ExperimentMatFiles');
+        experimentMatFilePath = fullfile(experimentMatFileFolder, strcat(char(ExpName),'_',Params.outputDataFolderName,'.mat'));
+    end
+    
+    expData = load(experimentMatFilePath);
+
+
+end
+
