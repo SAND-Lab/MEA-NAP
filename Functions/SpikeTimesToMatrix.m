99e72f42fb1c26327a68de6ebbaa8ad0dfc32134
timothysit <sitpakhang@gmail.com>
2024-10-23 16:37:19 +0100

    now does not rely on spike detection result structure

--- Functions/SpikeTimesToMatrix.m
+++ Functions/SpikeTimesToMatrix.m
@@ -1,5 +1,7 @@
-function [spikeMatrix] = SpikeTimesToMatrix(spikeTimes,spikeDetectionResult,method,Info)
-
+function [spikeMatrix] = SpikeTimesToMatrix(spikeTimes,fs,method,Info)
+    % Convert spike times to spike matrix 
+    % TODO: can replace spikeDetectionResult with just fs, since
+    % spikeDetectionResult may not always be available
     % Get spike times
     for i = 1:length(spikeTimes)
         if ~isempty(spikeTimes{1,i})
@@ -10,8 +12,7 @@ function [spikeMatrix] = SpikeTimesToMatrix(spikeTimes,spikeDetectionResult,meth
     % Convert spike times to matrix
     start_time = 0;
     end_time = Info.duration_s;
-    sampling_rate = spikeDetectionResult.params.fs; % number of samples per second
     spikeMatrix = spikeTimeToMatrix(spikeTimesStruct, ...
-        start_time, end_time, sampling_rate);
+        start_time, end_time, fs);
     
 end
