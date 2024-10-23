8d195adccaf6183bbc01b456eadcd8db1370a94c
timothysit <sitpakhang@gmail.com>
2024-10-23 16:35:33 +0100

    minor bug fix

--- Functions/util/loadExpData.m
+++ Functions/util/loadExpData.m
@@ -3,7 +3,7 @@ function expData = loadExpData(ExpName, Params, usePriorNetMet)
 %   Detailed explanation goes here
     if Params.priorAnalysis==1 && Params.startAnalysisStep==4 && usePriorNetMet
         experimentMatFileFolder = fullfile(Params.priorAnalysisPath, 'ExperimentMatFiles');
-        experimentMatFilePath = fullfile(experimentMatFileFolder, strcat(char(ExpName),'_',Params.priorAnalysisDate,'.mat'));
+        experimentMatFilePath = fullfile(experimentMatFileFolder, strcat(char(ExpName),'_',Params.priorAnalysisSubFolderName,'.mat'));
     else
         experimentMatFileFolder = fullfile(Params.outputDataFolder, Params.outputDataFolderName, 'ExperimentMatFiles');
         experimentMatFilePath = fullfile(experimentMatFileFolder, strcat(char(ExpName),'_',Params.outputDataFolderName,'.mat'));
