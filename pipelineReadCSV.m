db7a47adeca6a1c22b89f08500d629630a25b8a8
timothysit <sitpakhang@gmail.com>
2024-10-31 11:24:38 +0000

    added dealing with empty csv

--- pipelineReadCSV.m
+++ pipelineReadCSV.m
@@ -4,15 +4,20 @@ function csv_data = pipelineReadCSV(spreadsheet_filename, csvRange)
     opts = detectImportOptions(spreadsheet_filename);
     opts.Delimiter = ',';
     opts.VariableNamesLine = 1;
-    opts.VariableTypes{1} = 'char';  % this should be the recoding file name
-    opts.VariableTypes{2} = 'double';  % this should be the DIV
-    opts.VariableTypes{3} = 'char'; % this should be Group 
-    if length(opts.VariableNames) > 3
-        opts.VariableTypes{4} = 'char'; % this should be Ground
-    end 
-    opts.DataLines = csvRange; % read the data in the range [StartRow EndRow]
-    % csv_data = readtable(spreadsheet_filename, 'Delimiter','comma');
-    csv_data = readtable(spreadsheet_filename, opts);
+    
+    if length(opts.VariableTypes) < 3
+       csv_data = 0; 
+    else 
+        opts.VariableTypes{1} = 'char';  % this should be the recoding file name
+        opts.VariableTypes{2} = 'double';  % this should be the DIV
+        opts.VariableTypes{3} = 'char'; % this should be Group 
+        if length(opts.VariableNames) > 3
+            opts.VariableTypes{4} = 'char'; % this should be Ground
+        end 
+        opts.DataLines = csvRange; % read the data in the range [StartRow EndRow]
+        % csv_data = readtable(spreadsheet_filename, 'Delimiter','comma');
+        csv_data = readtable(spreadsheet_filename, opts);
+    end
     
 end
 
