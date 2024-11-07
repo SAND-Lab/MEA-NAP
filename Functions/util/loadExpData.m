bdda883067d0019793d672c8b62b9da772f6dda8
timothysit <sitpakhang@gmail.com>
2024-11-07 10:54:28 +0000

    now also used in step 3

--- Functions/util/loadExpData.m
+++ Functions/util/loadExpData.m
@@ -4,13 +4,24 @@ function expData = loadExpData(ExpName, Params, usePriorNetMet)
     if Params.priorAnalysis==1 && Params.startAnalysisStep==4 && usePriorNetMet
         experimentMatFileFolder = fullfile(Params.priorAnalysisPath, 'ExperimentMatFiles');
         experimentMatFilePath = fullfile(experimentMatFileFolder, strcat(char(ExpName),'_',Params.priorAnalysisSubFolderName,'.mat'));
+        expData = load(experimentMatFilePath);
+    elseif Params.priorAnalysis==1 && Params.startAnalysisStep==3
+        priorAnalysisExpMatFolder = fullfile(Params.priorAnalysisPath, 'ExperimentMatFiles');
+        spikeDataFname = strcat(char(ExpName(ExN)),'_',Params.priorAnalysisSubFolderName, '.mat');
+        spikeDataFpath = fullfile(priorAnalysisExpMatFolder, spikeDataFname);
+        if isfile(spikeDataFpath)
+            expData = load(spikeDataFpath, 'spikeTimes', 'Ephys', 'Info');
+        else 
+            % look for spike data in spike data folder 
+            spikeDataFpath = fullfile(Params.spikeDetectedData, ...
+                strcat([char(ExpName(ExN)) '_spikes.mat']));
+            expData = load(spikeDataFpath, 'spikeTimes', 'Info');
+        end 
     else
         experimentMatFileFolder = fullfile(Params.outputDataFolder, Params.outputDataFolderName, 'ExperimentMatFiles');
         experimentMatFilePath = fullfile(experimentMatFileFolder, strcat(char(ExpName),'_',Params.outputDataFolderName,'.mat'));
+        expData = load(experimentMatFilePath);
     end
     
-    expData = load(experimentMatFilePath);
-
-
 end
 
