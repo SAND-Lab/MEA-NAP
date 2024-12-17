cb359a05fae37cf2e4d523c92f252279bc64c16c
timothysit <sitpakhang@gmail.com>
2024-12-17 15:26:48 +0000

    updated axion 64 layout

--- Functions/getCoordsFromLayout.m
+++ Functions/getCoordsFromLayout.m
@@ -132,25 +132,36 @@ elseif strcmp(channelLayout, 'MCS59')
 
 
 elseif strcmp(channelLayout, 'Axion64')
-
-    channels = [11, 12, 13, 14, 15, 16, 17, 18, ... 
-            21, 22, 23, 24, 25, 26, 27, 28, ...
-            31, 32, 33, 34, 35, 36, 37, 38, ...
-            41, 42, 43, 44, 45, 46, 47, 48, ...
-            51, 52, 53, 54, 55, 56, 57, 58, ...
-            61, 62, 63, 64, 65, 66, 67, 68, ...,
-            71, 72, 73, 74, 75, 76, 77, 78, ...,
-            81, 82, 83, 84, 85, 86, 87, 88];
+    
+    channels = []; 
     coords = zeros(length(channels), 2);
-    coords(:, 2) = repmat(linspace(0, 1, 8), 1, 8);
-    coords(:, 1) = repelem(linspace(0, 1, 8), 1, 8);
+    
+    numRow = 8;
+    numCol = 8;
+    spacing_vals = linspace(0, 1, numRow);
+    
+    channelIdx = 1;
+    for rowIdx = 1:numRow 
+        for colIdx = 1:numCol 
+           channels(channelIdx) = colIdx * 10 + rowIdx;
+           coords(channelIdx, 1) = spacing_vals(colIdx);
+           coords(channelIdx, 2) = spacing_vals(rowIdx);
+           channelIdx = channelIdx + 1;
+       end 
+    end 
 
 elseif strcmp(channelLayout, 'Axion16')    
     
-    channels = [11, 12, 13, 14, ...
-            21, 22, 23, 24, ...
-            31, 32, 33, 34, ...
-            41, 42, 43, 44];
+    channels = []; 
+    numRow = 4;
+    numCol = 4;
+    
+    for rowIdx = 1:numRow 
+        for colIdx = 1:numCol 
+           channels(end+1) = colIdx * 10 + rowIdx;
+       end 
+    end 
+    
     coords = zeros(length(channels), 2);
     coords(:, 2) = repmat(linspace(0, 1, 4), 1, 4);
     coords(:, 1) = repelem(linspace(0, 1, 4), 1, 4);
