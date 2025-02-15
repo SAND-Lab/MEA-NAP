a0bd19ed4e252476653666461ca52223908b0fc8
timsit <sitpakhang@gmail.com>
2025-02-15 15:03:46 +0000

    updated _static path

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,8 @@ html_theme = 'furo'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
+html_static_path = ['_build/html/_static']
 
 # 2025-02-15 TIM: Trying to add custom css to get bold itatlics
 # These paths are either relative to html_static_path
